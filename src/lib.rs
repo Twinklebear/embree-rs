8152aecb9e2235986a1054ef44e6b908203c4c83
Will Usher <willusher.life@gmail.com>
2017-07-05 19:29:42 -0500

    Put the raw system lib bindings in the sys module

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
 
-pub mod embree;
-pub use embree::*;
+#[allow(non_upper_case_globals)]
+#[allow(non_camel_case_types)]
+#[allow(non_snake_case)]
+pub mod sys;
+pub use sys::*;
 
