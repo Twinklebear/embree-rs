9901e86f79b98bbc8030555ff41317553fb17f03
Will Usher <willusher.life@gmail.com>
2017-08-06 14:33:23 -0500

    Use the support lib window to display the triangle example

--- examples/triangle/src/main.rs
+++ examples/triangle/src/main.rs
@@ -1,9 +1,9 @@
 #![allow(dead_code)]
 
-extern crate image;
 extern crate embree;
+extern crate support;
 
-use std::{ptr, slice, f32, u32, iter};
+use std::{ptr, slice, f32, u32};
 
 #[repr(C)]
 struct Vertex {
@@ -20,6 +20,7 @@ struct Triangle {
 }
 
 fn main() {
+    let mut display = support::Display::new(512, 512, "triangle");
     unsafe {
         let device = embree::rtcNewDevice(ptr::null());
         let scene = embree::rtcDeviceNewScene(device, embree::RTCSceneFlags::RTC_SCENE_STATIC,
@@ -42,36 +43,30 @@ fn main() {
             tris[0] = Triangle { v0: 0, v1: 1, v2: 2 };
             embree::rtcUnmapBuffer(scene, geom_id, embree::RTCBufferType::RTC_INDEX_BUFFER);
         }
-
         embree::rtcCommit(scene);
 
-        let img_dims = 512;
-        let mut image: Vec<_> = iter::repeat(0u8).take(img_dims * img_dims * 3).collect();
-        // Render the scene
-        for j in 0..img_dims {
-            let y = -(j as f32 + 0.5) / img_dims as f32 + 0.5;
-            for i in 0..img_dims {
-                let x = (i as f32 + 0.5) / img_dims as f32 - 0.5;
-                let dir_len = f32::sqrt(x * x + y * y + 1.0);
-                let mut ray = embree::RTCRay::new(&[0.0, 0.5, 2.0],
-                                                  &[x / dir_len, y / dir_len, -1.0 / dir_len]);
-                embree::rtcIntersect(scene, &mut ray as *mut embree::RTCRay);
-                if ray.geomID != u32::MAX {
-                    image[(j * img_dims + i) * 3] = (ray.u * 255.0) as u8;
-                    image[(j * img_dims + i) * 3 + 1] = (ray.v * 255.0) as u8;
-                    image[(j * img_dims + i) * 3 + 2] = 0;
+        display.run(|image| {
+            let img_dims = image.dimensions();
+            // Render the scene
+            for j in 0..img_dims.1 {
+                let y = -(j as f32 + 0.5) / img_dims.1 as f32 + 0.5;
+                for i in 0..img_dims.0 {
+                    let x = (i as f32 + 0.5) / img_dims.0 as f32 - 0.5;
+                    let dir_len = f32::sqrt(x * x + y * y + 1.0);
+                    let mut ray = embree::RTCRay::new(&[0.0, 0.5, 2.0],
+                                                      &[x / dir_len, y / dir_len, -1.0 / dir_len]);
+                    embree::rtcIntersect(scene, &mut ray as *mut embree::RTCRay);
+                    if ray.geomID != u32::MAX {
+                        let mut p = image.get_pixel_mut(i, j);
+                        p.data[0] = (ray.u * 255.0) as u8;
+                        p.data[1] = (ray.v * 255.0) as u8;
+                        p.data[2] = 0;
+                    }
                 }
             }
-        }
+        });
         embree::rtcDeleteScene(scene);
         embree::rtcDeleteDevice(device);
-
-        match image::save_buffer("result.png", &image[..], img_dims as u32, img_dims as u32,
-                                 image::RGB(8))
-        {
-            Ok(_) => println!("Result saved to result.png"),
-            Err(e) => panic!("Error saving image: {}", e),
-        }
     }
 }
 
