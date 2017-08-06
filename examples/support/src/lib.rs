e6a6bfee15bfcfc98cb9fbb421308f31f16b133b
Will Usher <willusher.life@gmail.com>
2017-08-06 18:30:43 -0500

    add clamp method to support lib

--- examples/support/src/lib.rs
+++ examples/support/src/lib.rs
@@ -11,3 +11,8 @@ pub use vec3f::Vec3f;
 pub use camera::Camera;
 pub use display::Display;
 
+/// Clamp `x` to be between `min` and `max`
+pub fn clamp<T: PartialOrd>(x: T, min: T, max: T) -> T {
+    if x < min { min } else if x > max { max } else { x }
+}
+
