7cb619ef7615db3b0232d16d02ecf8408dbed978
Will Usher <willusher.life@gmail.com>
2017-08-11 19:10:46 -0500

    Use new wrappers to implement the triangle example

--- examples/triangle/src/main.rs
+++ examples/triangle/src/main.rs
@@ -2,72 +2,48 @@
 
 extern crate embree;
 extern crate support;
+extern crate cgmath;
 
-use std::{ptr, slice, f32, u32};
-
-#[repr(C)]
-struct Vertex {
-    x: f32,
-    y: f32,
-    z: f32,
-    a: f32,
-}
-#[repr(C)]
-struct Triangle {
-    v0: i32,
-    v1: i32,
-    v2: i32,
-}
+use std::{f32, u32};
+use cgmath::{Vector3, Vector4};
 
 fn main() {
     let mut display = support::Display::new(512, 512, "triangle");
-    unsafe {
-        let device = embree::rtcNewDevice(ptr::null());
-        let scene = embree::rtcDeviceNewScene(device, embree::RTCSceneFlags::RTC_SCENE_STATIC,
-                                              embree::RTCAlgorithmFlags::RTC_INTERSECT1);
+    let device = embree::Device::new();
+    let scene = embree::Scene::new(&device, embree::SCENE_STATIC, embree::INTERSECT1);
 
-        // Make a triangle
-        let geom_id = embree::rtcNewTriangleMesh2(scene, embree::RTCGeometryFlags::RTC_GEOMETRY_STATIC,
-                                                 1, 3, 1, 1);
-        {
-            let buf = embree::rtcMapBuffer(scene, geom_id, embree::RTCBufferType::RTC_VERTEX_BUFFER);
-            let mut verts: &mut [Vertex] = slice::from_raw_parts_mut(buf as *mut Vertex, 3);
-            verts[0] = Vertex { x: -1.0, y: 0.0, z: 0.0, a: 0.0 };
-            verts[1] = Vertex { x: 0.0, y: 1.0, z: 0.0, a: 0.0 };
-            verts[2] = Vertex { x: 1.0, y: 0.0, z: 0.0, a: 0.0 };
-            embree::rtcUnmapBuffer(scene, geom_id, embree::RTCBufferType::RTC_VERTEX_BUFFER);
-        }
-        {
-            let buf = embree::rtcMapBuffer(scene, geom_id, embree::RTCBufferType::RTC_INDEX_BUFFER);
-            let mut tris: &mut [Triangle] = slice::from_raw_parts_mut(buf as *mut Triangle, 1);
-            tris[0] = Triangle { v0: 0, v1: 1, v2: 2 };
-            embree::rtcUnmapBuffer(scene, geom_id, embree::RTCBufferType::RTC_INDEX_BUFFER);
-        }
-        embree::rtcCommit(scene);
+    // Make a triangle
+    let mut triangle = embree::TriangleMesh::unanimated(&scene, embree::STATIC, 1, 3);
+    {
+        let mut verts = triangle.vertex_buffer.map();
+        let mut tris = triangle.index_buffer.map();
+        verts[0] = Vector4::new(-1.0, 0.0, 0.0, 0.0);
+        verts[1] = Vector4::new(0.0, 1.0, 0.0, 0.0);
+        verts[2] = Vector4::new(1.0, 0.0, 0.0, 0.0);
 
-        display.run(|image, _, _| {
-            let img_dims = image.dimensions();
-            // Render the scene
-            for j in 0..img_dims.1 {
-                let y = -(j as f32 + 0.5) / img_dims.1 as f32 + 0.5;
-                for i in 0..img_dims.0 {
-                    let x = (i as f32 + 0.5) / img_dims.0 as f32 - 0.5;
-                    let dir_len = f32::sqrt(x * x + y * y + 1.0);
-                    let mut ray = embree::RTCRay::new(&[0.0, 0.5, 2.0],
-                                                      &[x / dir_len, y / dir_len, -1.0 / dir_len]);
-                    embree::rtcIntersect(scene, &mut ray as *mut embree::RTCRay);
-                    if ray.geomID != u32::MAX {
-                        let mut p = image.get_pixel_mut(i, j);
-                        p.data[0] = (ray.u * 255.0) as u8;
-                        p.data[1] = (ray.v * 255.0) as u8;
-                        p.data[2] = 0;
-                    }
+        tris[0] = Vector3::new(0, 1, 2);
+    }
+    scene.commit();
+
+    display.run(|image, _, _| {
+        let img_dims = image.dimensions();
+        // Render the scene
+        for j in 0..img_dims.1 {
+            let y = -(j as f32 + 0.5) / img_dims.1 as f32 + 0.5;
+            for i in 0..img_dims.0 {
+                let x = (i as f32 + 0.5) / img_dims.0 as f32 - 0.5;
+                let dir_len = f32::sqrt(x * x + y * y + 1.0);
+                let mut ray = embree::Ray::new(&[0.0, 0.5, 2.0],
+                                               &[x / dir_len, y / dir_len, -1.0 / dir_len]);
+                scene.intersect(&mut ray);
+                if ray.geomID != u32::MAX {
+                    let mut p = image.get_pixel_mut(i, j);
+                    p.data[0] = (ray.u * 255.0) as u8;
+                    p.data[1] = (ray.v * 255.0) as u8;
+                    p.data[2] = 0;
                 }
             }
-        });
-        embree::rtcDeleteGeometry(scene, geom_id);
-        embree::rtcDeleteScene(scene);
-        embree::rtcDeleteDevice(device);
-    }
+        }
+    });
 }
 
