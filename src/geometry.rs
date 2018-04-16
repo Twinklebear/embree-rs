e6c8b7b0df7e75af9ea199625a1ba503c8995e09
Will Usher <willusher.life@gmail.com>
2018-04-15 18:28:47 -0600

    use the geometry type to get the handle back

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,30 +1,19 @@
-use std::cmp::{PartialEq, Eq};
-
-/// TODO: The geometry ids are per-scene now, so we must change
-/// how we handle this trait.
+use sys::*;
 
 /// Geometry trait implemented by all Embree Geometry types
 pub trait Geometry {
-    fn geom_id(&self) -> u32;
+    fn handle(&self) -> RTCGeometry;
+    fn commit(&mut self) {
+        unsafe { rtcCommitGeometry(self.handle()); }
+    }
 }
 
 impl PartialEq<Geometry> for Geometry {
     fn eq(&self, other: &Geometry) -> bool {
-        self.geom_id() == other.geom_id()
-    }
-}
-
-impl PartialEq<u32> for Geometry {
-    fn eq(&self, other: &u32) -> bool {
-        self.geom_id() == *other
-    }
-}
-
-impl PartialEq<Geometry> for u32 {
-    fn eq(&self, other: &Geometry) -> bool {
-        *self == other.geom_id()
+        self.handle() == other.handle()
     }
 }
 
 impl Eq for Geometry {}
+
 
