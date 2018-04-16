5c13e7c895c482615664d3cf02b90c5f51694f40
Will Usher <willusher.life@gmail.com>
2018-04-15 18:30:00 -0600

    Make scene generic on the geometries added, keep map of id -> geometry

--- src/scene.rs
+++ src/scene.rs
@@ -1,8 +1,9 @@
 use std::marker::PhantomData;
+use std::collections::HashMap;
 
 use sys::*;
 use device::Device;
-use triangle_mesh::TriangleMesh;
+use geometry::Geometry;
 use ray::{Ray, RayHit, IntersectContext};
 
 pub struct Scene<'a> {
@@ -10,16 +11,30 @@ pub struct Scene<'a> {
     /// We don't need to actually keep a reference to the device,
     /// we just need to track its lifetime for correctness
     device: PhantomData<&'a Device>,
+    // Technically this should be a map, as the ids can be re-used
+    geometry: HashMap<u32, &'a Geometry>,
 }
 impl<'a> Scene<'a> {
     pub fn new(device: &'a Device) -> Scene {
         Scene {
             handle: unsafe { rtcNewScene(device.handle) },
-            device: PhantomData
+            device: PhantomData,
+            geometry: HashMap::new(),                
         }
     }
-    pub fn attach_geometry(&mut self, mesh: &TriangleMesh) -> u32 {
-        unsafe { rtcAttachGeometry(self.handle, mesh.handle) }
+    /// Attach a new geometry to the scene. Returns the scene local ID which
+    /// can than be used to find the hit geometry from the ray ID member.
+    pub fn attach_geometry(&mut self, mesh: &'a Geometry) -> u32 {
+        let id = unsafe { rtcAttachGeometry(self.handle, mesh.handle()) };
+        self.geometry.insert(id, mesh);
+        id
+    }
+    /// Look up a geometry in the scene by the ID returned from `attach_geometry`
+    pub fn get_geometry(&self, id: u32) -> Option<&'a Geometry> {
+        match self.geometry.get(&id) {
+            Some(g) => Some(*g),
+            None => None,
+        }
     }
     pub fn commit(&self) {
         unsafe { rtcCommitScene(self.handle); }
