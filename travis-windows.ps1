f5e4f5a0149a429ed5d9ebb3bfb970f1c41799d2
Will Usher <willusher.life@gmail.com>
2019-02-13 23:18:29 -0700

    script for windows debugging

--- travis-windows.ps1
+++ travis-windows.ps1
@@ -1,11 +1,17 @@
 $EMBREE_VERSION=$args[0]
 $env:EMBREE_DIR="$env:HOME\embree-$EMBREE_VERSION.x64.vc14.windows"
 
+Write-Output "Travis script debug test"
+Write-Host "Travis script debug test"
+
 # build the crate
 cargo build
 if (!$?) {
 	exit 1
 }
+
+Write-Output "Built crate"
+Write-Host "Built crate"
 
 cargo test
 if (!$?) {
@@ -16,6 +22,7 @@ if (!$?) {
 cd examples
 Get-ChildItem .\ -Directory | ForEach-Object {
 	Write-Output $_
+	Write-Host $_
 	cd $_
 	cargo build
 	if (!$?) {
