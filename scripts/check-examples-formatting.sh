732b957429cae5b0d66320e5bd03cd6570bca2b2
Will Usher <will.usher@hey.com>
2021-02-05 22:12:14 -0700

    missing script

--- scripts/check-examples-formatting.sh
+++ scripts/check-examples-formatting.sh
@@ -3,12 +3,11 @@
 # build the examples
 cd examples
 for d in `ls ./`; do
-	cd $d
-	pwd
-	cargo fmt -- --check
-	if [[ "$?" != "0" ]]; then
-		exit 1
-	fi
-	cd ../
+    cd $d
+    pwd
+    cargo fmt -- --check
+    if [[ "$?" != "0" ]]; then
+        exit 1
+    fi
+    cd ../
 done
-
