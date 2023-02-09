2b7b18e2725006bab92d41afacbced2280dfed27
matthiascy <matthiasychen@gmail.com>
2023-02-09 11:28:53 +0100

    remove RTCError wrapper

--- src/error.rs
+++ src/error.rs
@@ -1,32 +1,22 @@
 use std::fmt::Display;
 
-use crate::sys;
+use crate::sys::RTCEqrror;
 
-/// Thin wrapper around Embree's `RTCError`.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct Error(sys::RTCError);
-
-impl Display for Error {
+impl Display for RTCError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self.0 {
-            sys::RTCError::NONE => write!(f, "No error"),
-            sys::RTCError::UNKNOWN => write!(f, "Unknown error"),
-            sys::RTCError::INVALID_ARGUMENT => write!(f, "Invalid argument"),
-            sys::RTCError::INVALID_OPERATION => {
+        match self {
+            RTCError::NONE => write!(f, "No error"),
+            RTCError::UNKNOWN => write!(f, "Unknown error"),
+            RTCError::INVALID_ARGUMENT => write!(f, "Invalid argument"),
+            RTCError::INVALID_OPERATION => {
                 write!(f, "Invalid operation.")
             }
-            sys::RTCError::OUT_OF_MEMORY => write!(f, "Out of memory"),
-            sys::RTCError::UNSUPPORTED_CPU => write!(f, "Unsupported CPU"),
-            sys::RTCError::CANCELLED => write!(
+            RTCError::OUT_OF_MEMORY => write!(f, "Out of memory"),
+            RTCError::UNSUPPORTED_CPU => write!(f, "Unsupported CPU"),
+            RTCError::CANCELLED => write!(
                 f,
                 "Cancelled by a memory monitor callback or progress monitor callback function"
             ),
         }
-    }
-}
-
-impl From<sys::RTCError> for Error {
-    fn from(err: sys::RTCError) -> Self {
-        Self(err)
     }
 }
